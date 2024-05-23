dc647a5e528bdbbd8abb21f735ea9dfaf2855361
lynnerv <aythor_briceno@usmp.pe>
2024-05-22 20:58:08 -0500

    inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BladeRunner2077.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
